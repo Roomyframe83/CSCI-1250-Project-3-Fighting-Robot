bc53e589b52bed888602fedd4cd53ae72b0d302d
Roomyframe83 <150959344+Roomyframe83@users.noreply.github.com>
2023-11-17 11:04:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RobotCaveFightTestArena {
+	internal class Program {
+		static void Main(string[] args) {
+			//Create bots
+			TrainingDummy trainingDummy = new TrainingDummy();
+			TrainingDummy trainingDummy1 = new TrainingDummy();
+
+			//Create a new arena
+			Arena arena = new Arena(trainingDummy, trainingDummy1);
+
+			arena.RunBattle();
+		}
+	}
+}
